41225ad744fb3ccf1b6765f4dc4db36b72b5cdae
Chrueso <81568143+Chrueso@users.noreply.github.com>
2024-01-22 08:20:35 +0800

    Update game.js

--- MyVersion2/game.js
+++ MyVersion2/game.js
@@ -4,13 +4,13 @@ let levelConfig = [
     levelPositioning: [
       {
         type: "HS",
-        x: 0,
-        y: 0,
+        x: 5,
+        y: 5,
       },
       {
         type: "HL",
-        x: 2,
-        y: 1,
+        x: 4,
+        y: 3,
       },
       {
         type: "VS",
@@ -19,12 +19,12 @@ let levelConfig = [
       },
       {
         type: "VL",
-        x: 1,
+        x: 2,
         y: 3,
       },
       {
         type: "P",
-        x: 3,
+        x: 0,
         y: 3,
       },
     ],
